bc5cbbad478712f673652ac4af0d815dffd3f667
adielcahana <adielcahana@gmail.com>
2018-03-27 18:05:22 +0300

    lp solver using pulp

---
+++ lpsolver.py
@@ -0,0 +1,52 @@
+import numpy as np
+from pulp import *
+
+
+def solve(probs, hospitals_order, students):
+    shape = probs.shape
+    P = LpVariable.dicts(name='P', indexs=((i, j) for i in range(shape[0]) for j in range(shape[1])),
+                          lowBound=0.0, upBound=1.0, cat='Continuous')
+    problem = LpProblem('students allocation', LpMaximize)
+    # normalization constraint
+    for row in range(shape[0]):
+        problem += lpSum(P[(row, j)] for j in range(shape[1])) <= 1.0
+    # seats constraints
+    columns_sum = np.sum(probs, axis=0)
+    for col in range(shape[1]):
+        problem += lpSum(P[(i, col)] for i in range(shape[0])) <= columns_sum[col]
+    # happiness constraint
+    coeff = get_happiness_coeff(hospitals_order, students)
+    happiness = [np.dot(probs[i], coeff[i]) for i in range(shape[0])]
+    for row in range(shape[0]):
+        problem += lpSum(P[(row, j)] * coeff[row][j] for j in range(shape[1])) >= happiness[row]
+    # objective function
+    problem += lpSum(P[(i, j)] * coeff[i][j] for i in range(shape[0]) for j in range(shape[1]))
+
+    print("before: " + str(np.sum(happiness)))
+    problem.solve(solver=)
+    print(LpStatus[problem.status])
+
+    if problem.status == LpStatusOptimal:
+        print("after: " + str(pulp.value(problem.objective)))
+        new_probs = np.zeros((len(students), len(hospitals_order)))
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                new_probs[i][j] = P[(i, j)].varValue
+        return new_probs
+
+
+
+def get_happiness_coeff(hospitals_order, students):
+    coeff = np.zeros((len(students), len(hospitals_order)))
+    i = 0
+    for student in students:
+        m = len(hospitals_order)
+        j = 0
+        for priority in student.priorities:
+            coeff[i][hospitals_order[priority]] = (m - j)**2
+            j += 1
+        i += 1
+    return coeff
+
+
+
--- tests.py
+++ tests.py
@@ -1,20 +1,27 @@
 import time
 
+import numpy as np
+
 import assignments
 import randomizer
 from data import Hospitals
+from lpsolver import solve
 
+np.random.seed(1)
 hospitals = Hospitals.from_excel("res/hospital_list_test.xlsx")
 students = randomizer.create_random_students(500, hospitals.names)
 start = time.time()
 probs, order = assignments.expected_hat(students, hospitals, 100)
-print(time.time() - start)
-# columns_sum = np.sum(probs, axis=0)
-# for name in hospitals.names:
-#     if columns_sum[order[name]] != hospitals[name] * 100:
-#         print('error')
+solution = solve(probs, order, students)
 
-# rows_sum = np.sum(probs, axis=1)
-# for i in range(len(rows_sum)):
-#     if rows_sum[i] - 1.0 >= 0.000001:
-#         print('error')
+print(time.time() - start)
+
+columns_sum = np.sum(solution, axis=0)
+for name in hospitals.names:
+    if columns_sum[order[name]] - hospitals[name] >= 0.0001:
+        print('error')
+
+rows_sum = np.sum(solution, axis=1)
+for i in range(len(rows_sum)):
+    if rows_sum[i] - 1.0 >= 0.000001:
+        print('error')
