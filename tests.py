9ffd3a54d18ecaaa459517dfc776887afe5eb9f6
ariel262626 <ariel262626@walla.co.il>
2018-04-03 12:55:41 +0300

    Birkhoff algo

--- tests.py
+++ tests.py
@@ -5,14 +5,19 @@ import numpy as np
 import assignments
 import randomizer
 from data import Hospitals
-from lpsolver import solve
+from lpsolver import solve, normalize_new_probs, birkhoff_algo, lottery
 
 np.random.seed(1)
-hospitals = Hospitals.from_excel("res/hospital_list_test.xlsx")
-students = randomizer.create_random_students(500, hospitals.names)
+hospitals = Hospitals.from_excel("res/hospital_list_test1.xlsx")
+students = randomizer.create_random_students(10, hospitals.names)
 start = time.time()
-probs, order = assignments.expected_hat(students, hospitals, 100000)
+probs, order = assignments.expected_hat(students, hospitals, 20)
 solution = solve(probs, order, students)
+d = normalize_new_probs(solution, hospitals._seats, hospitals.names, order)
+birkhoff_list = birkhoff_algo(d)
+choice = lottery(birkhoff_list)
+print("\n\nwe choose: \n", birkhoff_list[choice[0]][1])
+
 
 print(time.time() - start)
 
